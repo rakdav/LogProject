e477d554af5c494960674b31af2c3b207a1818fa
rakdav <rakdav@inbox.ru>
2025-01-29 19:56:39 +0200

    Last1

--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,2 +1,25 @@
-﻿ar builder = new ConfigurationBuilder();
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PracticeProject;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("connection.json");
+var config = builder.Build();
+var connectionString = config.GetConnectionString("DefaultConnection");
+var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+var version = new MySqlServerVersion(new Version(8, 0, 25));
+var options = optionsBuilder.UseMySql(connectionString, version).Options;
+using (UserDbContext db = new UserDbContext(options))
+{
+    Person person = new Person() { Email = "fast@mail.ru", Name = "Fast", Password = "Qwerty" };
+    db.People.Add(person);
+    db.SaveChanges();
+}
+
+
+
+
+
 
--- PracticeProject/UserDbContext.cs
+++ PracticeProject/UserDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,28 @@ namespace PracticeProject
 {
     public class UserDbContext:DbContext
     {
+        private StreamWriter infoLog = new StreamWriter("infoLog.log",true);
+        private StreamWriter warningLog = new StreamWriter("warningLog.log", true);
+        private StreamWriter criticalLog = new StreamWriter("criticalLog.log", true);
         public UserDbContext(DbContextOptions options) : base(options)
         {
             Database.EnsureCreated();
         }
         public DbSet<Person> People { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(infoLog.WriteLine, LogLevel.Information);
+            optionsBuilder.LogTo(warningLog.WriteLine, LogLevel.Warning);
+            optionsBuilder.LogTo(criticalLog.WriteLine, LogLevel.Critical);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            infoLog.Dispose();
+            warningLog.Dispose();
+            criticalLog.Dispose();
+        }
     }
 }
